c2e2544d5cef5440d49d306412ebe9305d30f6e3
varmamsp <varmamsp111@gmail.com>
2020-06-17 01:02:54 +0530

    Add code to start moor server

---
+++ lib/background/moor_server/main.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:moor/isolate.dart';
+import 'package:moor/moor.dart';
+import 'package:moor_ffi/moor_ffi.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+DatabaseConnection _backgroundConnection() {
+  final database = LazyDatabase(
+    () async {
+      final dbFolder = await getApplicationDocumentsDirectory();
+      final file = File(join(dbFolder.path, 'db.sqlite'));
+      return VmDatabase(file);
+    },
+  );
+
+  return DatabaseConnection.fromExecutor(database);
+}
+
+Future<MoorIsolate> _newIsolate() async {
+  final moorIsolate = await MoorIsolate.spawn(_backgroundConnection);
+  IsolateNameServer.registerPortWithName(
+    moorIsolate.connectPort,
+    'MOOR_ISOLATE',
+  );
+
+  return moorIsolate;
+}
+
+Future<MoorIsolate> getMoorIsolate() {
+  final sendPort = IsolateNameServer.lookupPortByName('MOOR_ISOLATE');
+  return sendPort != null
+      ? MoorIsolate.fromConnectPort(sendPort)
+      : _newIsolate();
+}
