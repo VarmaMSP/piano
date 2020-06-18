64b6125dbb6feeb259585729e19f4c952602007d
varmamsp <varmamsp111@gmail.com>
2020-06-18 22:17:04 +0530

    Remove cast_to_non_type from build rules to ignore

--- lib/model/user.g.dart
+++ lib/model/user.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-// ignore_for_file: unnecessary_this, cast_to_non_type, return_of_invalid_type, constant_identifier_names, prefer_const_constructors_in_immutables, sort_unnamed_constructors_first, join_return_with_assignment
+// ignore_for_file: unnecessary_this, return_of_invalid_type, constant_identifier_names, prefer_const_constructors_in_immutables, sort_unnamed_constructors_first, join_return_with_assignment
 
 part of 'user.dart';
 
