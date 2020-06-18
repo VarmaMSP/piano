a8511feb51f0cbb250c409403ecd996e1281304a
varmamsp <varmamsp111@gmail.com>
2020-06-18 16:40:01 +0530

    Ignore some linter rules in generated files

--- lib/model/api_response.g.dart
+++ lib/model/api_response.g.dart
@@ -1,5 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: unnecessary_this, cast_to_non_type, return_of_invalid_type, constant_identifier_names, prefer_const_constructors_in_immutables, sort_unnamed_constructors_first, join_return_with_assignment
+
 part of 'api_response.dart';
 
 // **************************************************************************
